da4ad52a27ad6afe9047a0a452b85de4f629a1c7
Paless Domonkos <palessdomonkos@gmail.com>
2023-11-28 13:54:36 +0100

    works again, with utf-8, TODO read_file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::{
-    cursor::{self, EnableBlinking, MoveTo, MoveDown, MoveToColumn, RestorePosition, SavePosition},
+    cursor::{self, EnableBlinking, MoveDown, MoveTo, MoveToColumn, RestorePosition, SavePosition},
     event::{self, Event::Key, KeyCode, KeyEvent, KeyModifiers},
     execute,
     terminal::{
@@ -7,12 +7,35 @@ use crossterm::{
         LeaveAlternateScreen,
     },
 };
-use std::io::{self, Write};
+use std::{io::{self, Write}, env::args};
 
 struct BufferContext {
-    buf: Vec<Vec<u8>>,
+    buf: Vec<Vec<char>>,
     last_x: u16,
     mode: BufferMode,
+}
+
+impl BufferContext {
+    fn write_buf_to_screen(&self) -> io::Result<()> {
+        execute!(io::stdout(), MoveTo(0, 0))?;
+        for (i, line) in self.buf.iter().enumerate() {
+            execute!(io::stdout(), MoveTo(0, i as u16))?;
+            let mut final_line = vec![];
+            let mut buf = [0; 4];
+            for char in line {
+                char.encode_utf8(&mut buf);
+                for byte in buf {
+                    final_line.push(byte);
+                }
+            }
+            io::stdout().write_all(&final_line)?;
+            execute!(io::stdout(), MoveDown(1))?;
+        }
+        Ok(())
+    }
+    fn read_file(&mut self, path: String) -> io::Result<()> {
+        Ok(())
+    }
 }
 
 enum BufferMode {
@@ -20,27 +43,42 @@ enum BufferMode {
     Insert,
 }
 
+fn overwrite_char(pos: (u16, u16), context: &mut BufferContext, c: char) -> io::Result<()> {
+    let (x, y) = pos;
+    let buffer = &mut context.buf;
+
+    let deletion_length = buffer[y as usize][x as usize..buffer[y as usize].len()]
+        .iter()
+        .map(|x| x.len_utf8())
+        .sum::<usize>();
+
+    let char_buf = &mut [0; 4];
+    c.encode_utf8(char_buf);
+
+    io::stdout().write_all(vec![0; deletion_length].as_ref())?;
+    execute!(io::stdout(), MoveTo(x, y))?;
+    io::stdout().write_all(&char_buf[0..c.len_utf8()])?;
+
+    Ok(())
+}
+
 fn write_char(context: &mut BufferContext, c: char) -> io::Result<()> {
     let buffer = &mut context.buf;
     let (x, y) = cursor::position()?;
     let (size_x, _size_y) = terminal::size()?;
     if x < size_x - 1 {
-        execute!(io::stdout(), MoveTo(x + 1, y))?;
-        buffer[y as usize].push(c as u8);
+        buffer[y as usize].insert(x as usize, c);
     } else {
-        execute!(io::stdout(), MoveTo(1, y + 1))?;
+        execute!(io::stdout(), MoveTo(0, y + 1))?;
         buffer.push(vec![]);
-        buffer[y as usize].push(c as u8);
+        buffer[(y + 1) as usize].push(c);
     }
-    
-    
-    let (x, y) = cursor::position()?;
-    execute!(io::stdout(), MoveTo(0, y))?;
-    io::stdout().write_all(vec![0; buffer[y as usize].len()].as_ref())?;
-    io::stdout().write_all(buffer[y as usize].as_ref())?;
-    execute!(io::stdout(), MoveTo(x, y))?;
 
-    context.last_x = x;
+    let (x, y) = cursor::position()?;
+    overwrite_char((x, y), context, c)?;
+
+    execute!(io::stdout(), MoveTo(x + 1, y))?;
+    context.last_x = cursor::position()?.0;
 
     Ok(())
 }
@@ -49,35 +87,53 @@ fn delete_char(context: &mut BufferContext) -> io::Result<()> {
     let buffer = &mut context.buf;
 
     let (x, y) = cursor::position()?;
-    if x != 0 {
+    let mut deletion_length = 0;
+    if x > 0 {
+        deletion_length = buffer[y as usize][(x - 1) as usize..buffer[y as usize].len()]
+            .iter()
+            .map(|x| x.len_utf8())
+            .sum::<usize>();
         buffer[y as usize].remove((x - 1) as usize);
         execute!(io::stdout(), MoveTo(x - 1, y))?;
+    } else {
+        if buffer.len() != 1 && buffer[y as usize].len() < 1 {
+            buffer.remove(y as usize);
+            execute!(
+                io::stdout(),
+                MoveTo(buffer[y as usize - 1].len() as u16, y - 1)
+            )?;
+        }
     }
 
     let (x, y) = cursor::position()?;
-    execute!(io::stdout(), MoveTo(x, y))?;
-    io::stdout().write_all(
-        vec![' ' as u8; buffer[y as usize][x as usize..buffer[y as usize].len()].len() + 1]
-            .as_ref(),
-    )?;
-    execute!(io::stdout(), MoveTo(0, y))?;
-    io::stdout().write_all(&buffer[y as usize][x as usize..buffer[y as usize].len()])?;
+
+    io::stdout().write_all(vec![b' '; deletion_length].as_ref())?;
+
     execute!(io::stdout(), MoveTo(x, y))?;
 
-    if buffer.len() != 1 && buffer[y as usize].len() < 1 {
-        buffer.remove(y as usize);
-        execute!(
-            io::stdout(),
-            MoveTo(buffer[y as usize - 1].len() as u16, y - 1)
-        )?;
+    let mut final_printout = vec![];
+    for char in buffer[y as usize][x as usize..buffer[y as usize].len()].iter() {
+        let char_buf = &mut [0; 4];
+        char.encode_utf8(char_buf);
+        for byte in char_buf {
+            final_printout.push(*byte);
+        }
     }
+    io::stdout().write_all(&final_printout.as_ref())?;
 
+    execute!(io::stdout(), MoveTo(x, y))?;
+
+    let (x, _) = cursor::position()?;
     context.last_x = x;
 
     Ok(())
 }
 
-fn movement(code: KeyCode, _modifiers: KeyModifiers, context: &mut BufferContext) -> io::Result<()> {
+fn movement(
+    code: KeyCode,
+    _modifiers: KeyModifiers,
+    context: &mut BufferContext,
+) -> io::Result<()> {
     let buffer = &context.buf;
     let (x, y) = cursor::position()?;
     match code {
@@ -96,9 +152,11 @@ fn movement(code: KeyCode, _modifiers: KeyModifiers, context: &mut BufferContext
         KeyCode::Up => {
             if y > 0 {
                 if (context.last_x as usize) > buffer[y as usize - 1].len() {
-                    execute!(io::stdout(), MoveTo(buffer[y as usize - 1].len() as u16, y - 1))?;
-                }
-                else {
+                    execute!(
+                        io::stdout(),
+                        MoveTo(buffer[y as usize - 1].len() as u16, y - 1)
+                    )?;
+                } else {
                     execute!(io::stdout(), MoveTo(context.last_x, y - 1))?;
                 }
             }
@@ -106,9 +164,11 @@ fn movement(code: KeyCode, _modifiers: KeyModifiers, context: &mut BufferContext
         KeyCode::Down => {
             if (y as usize) < buffer.len() - 1 {
                 if (context.last_x as usize) > buffer[y as usize + 1].len() {
-                    execute!(io::stdout(), MoveTo(buffer[y as usize + 1].len() as u16, y + 1))?;
-                }
-                else {
+                    execute!(
+                        io::stdout(),
+                        MoveTo(buffer[y as usize + 1].len() as u16, y + 1)
+                    )?;
+                } else {
                     execute!(io::stdout(), MoveTo(context.last_x, y + 1))?;
                 }
             }
@@ -119,7 +179,11 @@ fn movement(code: KeyCode, _modifiers: KeyModifiers, context: &mut BufferContext
 }
 
 fn main() -> std::io::Result<()> {
-    let mut context = BufferContext { buf: vec![vec![]], last_x: 0, mode: BufferMode::Insert };
+    let mut context = BufferContext {
+        buf: vec![vec![]],
+        last_x: 0,
+        mode: BufferMode::Insert,
+    };
     execute!(
         std::io::stdout(),
         SavePosition,
@@ -131,11 +195,11 @@ fn main() -> std::io::Result<()> {
     )?;
     terminal::enable_raw_mode()?;
 
-    execute!(io::stdout(), MoveTo(0, 0))?;
-    for (i, line) in context.buf.iter().enumerate() {
-        execute!(io::stdout(), MoveTo(0, i as u16))?;
-        io::stdout().write_all(line)?;
-    }
+    let mut args = args();
+    let path = args.nth(1).unwrap();
+
+    context.write_buf_to_screen()?;
+
     execute!(
         io::stdout(),
         MoveTo(
